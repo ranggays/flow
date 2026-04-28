8042d509c102c2cdd26b0e0b5ffde5a362227c2c
ranggays <ranggayoviesaputra@gmail.com>
2026-04-28 15:44:53 +0700

    fix: remove hydration warning

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -16,12 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
       </head>
       <body
         className={`${inter.className} bg-[#f6f6f8] dark:bg-[#121022] text-slate-900 dark:text-slate-100`}
+        suppressHydrationWarning
       >
         <ThemeProvider
           attribute="class"
